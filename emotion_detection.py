679832337595ebc41b91fe5953d2da8eb49829ad
keenan-n-dev <elexiakeenan@gmail.com>
2026-09-05 05:13:06 -0400

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,39 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests
 import json
-# Define a function named emotion_detector that takes a string input (text_to_analyse)
+
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' # URL of the emotion
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the headers required for the API
-    response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
-    return response.text
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    # Envoi de la requête
+    response = requests.post(url, json=myobj, headers=header)
+    
+    # Transformation de la réponse texte en dictionnaire Python
+    formatted_response = json.loads(response.text)
+    
+    # Extraction du dictionnaire contenant spécifiquement les émotions et leurs scores
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    # Récupération des scores individuels
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    
+    # Identification de l'émotion dominante grâce à la fonction max()
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    # Formatage du dictionnaire de sortie selon les instructions
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
+    # On retourne le dictionnaire et non plus response.text
+    return result
